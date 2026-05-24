1c17a9d972ad9019b79bba9aeafe49addee824f0
mujiayi2026 <mujiayi2026@users.noreply.github.com>
2026-05-24 21:06:43 +0800

    完善LED的注释

--- USER/LED/led.c
+++ USER/LED/led.c
@@ -10,3 +10,9 @@ void LedInit()
 	HAL_GPIO_WritePin(GPIOB,GPIO_PIN_1,GPIO_PIN_SET);
 }
 
+
+//LED操作函数
+//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_RESET);   //将PB0置低
+//HAL_GPIO_WritePin(GPIOB,GPIO_PIN_0,GPIO_PIN_SET);     //将PB0置高
+//HAL_GPIO_TogglePin(GPIOB,GPIO_PIN_0);                 //将PB0取反
+//GPIOB->ODR                                            //PB端口的寄存器，可进行位操作
